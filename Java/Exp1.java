c26e70870ac18a75d1ff55ee7aa236a28dc3161f
UshaKiran31 <143540281+UshaKiran31@users.noreply.github.com>
2023-08-30 00:41:06 +0530

    Update Exp1.java

--- Java/Exp1.java
+++ Java/Exp1.java
@@ -64,6 +64,8 @@ class Invoice
             System.out.println("Item Details");
             System.out.println("Part number:"+iv.getPart_number());
             System.out.println("Part description:"+iv.getPart_description());
+            System.out.println("Part description:"+iv.getPart_description());
+            System.out.println("Quantity:"+iv.getQuantity());
             System.out.println("Total Billing Amount:"+iv.getInvoiceAmount());   
            }
    }
